6a12dff251b4014fa2b3c6912d7a1eb0cc3b7351
sankitdev <ankitsingh635101@gmail.com>
2025-01-27 17:51:31 +0530

    feat: integrate RegisterUserDialog into student page with registration button and state management

--- app/dashboard/student/page.tsx
+++ app/dashboard/student/page.tsx
@@ -1,3 +1,17 @@
+"use client";
+import { RegisterUserDialog } from "@/components/register-user-dialog";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 export default function Page() {
-  return <>Student is here</>;
+  const [isOpen, setIsOpen] = useState(false);
+  const handleRegsiter = () => {
+    setIsOpen(true);
+  };
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Button onClick={handleRegsiter}>Register</Button>
+      <RegisterUserDialog isOpen={isOpen} onOpenChange={setIsOpen} />
+    </div>
+  );
 }
