3234f82b3f5794abb0a917d47700fcde6731a6dc
sankitdev <ankitsingh635101@gmail.com>
2025-01-27 15:15:43 +0530

    feat: add dashboard pages for HOD, staff, and student with basic content

---
+++ app/dashboard/hod/page.tsx
@@ -0,0 +1,10 @@
+import { RegisterUserForm } from "@/components/registerUser";
+
+export default function Page() {
+  return (
+    <>
+      HOD is here
+      <RegisterUserForm />
+    </>
+  );
+}
