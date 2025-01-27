e571825b783eccef2ddd2b272cf16ede948b59cd
sankitdev <ankitsingh635101@gmail.com>
2025-01-27 15:16:12 +0530

    feat: update calendar component layout and event date format; add new dependencies for form handling and validation

--- app/dashboard/calender/page.tsx
+++ app/dashboard/calender/page.tsx
@@ -4,14 +4,14 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 
 export default function Calendar() {
   return (
-    <div className="max-w-4xl mx-auto p-4 shadow-lg rounded-lg">
+    <div className="w-full md:w-[80%] md:h-[90%] mx-auto py-4 shadow-lg rounded-lg">
       <h1 className="text-2xl font-bold text-center mb-6">Leave Calendar</h1>
       <div className="overflow-hidden rounded-lg">
         <FullCalendar
           plugins={[dayGridPlugin]}
           initialView="dayGridMonth"
           events={[
-            { title: "Event 1", date: "2025-01-25" },
+            { title: "Event 1", start: "2025-01-25", end: "2025-01-29" },
             { title: "Event 2", date: "2025-01-28" },
           ]}
           height="auto"
