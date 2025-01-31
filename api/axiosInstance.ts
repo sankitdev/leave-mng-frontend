c902af49b38eff612a3241bab9312a2624f8cd83
sankitdev <ankitsingh635101@gmail.com>
2025-01-31 16:33:42 +0530

    fix: update axios base URL to use production endpoint

--- api/axiosInstance.ts
+++ api/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    "https://leave-mng-node.onrender.com",
   withCredentials: true,
 });
 
